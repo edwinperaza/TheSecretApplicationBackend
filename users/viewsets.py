3bad22fa28798d22839ccae4e4dc093e6664dd07
Cristian <cristian@magnet.cl>
2018-02-11 21:54:15 -0300

    no permissions in viewset user

--- users/viewsets.py
+++ users/viewsets.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import PasswordResetForm
 
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import mixins
@@ -20,6 +20,7 @@ class UserViewSet(
         mixins.DestroyModelMixin):
     model = User
     serializer_class = UserCreateSerializer
+    permission_classes = (AllowAny,)
 
     def create(self, request, *args, **kwargs):
         user_serializer = self.get_serializer(data=request.data)
